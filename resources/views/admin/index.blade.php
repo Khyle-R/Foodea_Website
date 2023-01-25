431514bb8a7d2b4525fd85b3c477c205a09b52ee
Murrzzz <rogermooresangol@gmail.com>
2023-01-25 08:28:38 -0800

    small changes
    Fix the routes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -119,7 +119,7 @@
             <span class="nav-link">Navigation</span>
           </li>
           <li class="nav-item menu-items">
-            <a class="nav-link" href="index.html">
+            <a class="nav-link" href="/index">
               <span class="menu-icon">
                 <i class="mdi mdi-view-dashboard"></i>
               </span>
@@ -171,7 +171,7 @@
             </a>
           </li>
           <li class="nav-item menu-items">
-            <a class="nav-link" href="admin_history.html">
+            <a class="nav-link" href="/admin_history">
               <span class="menu-icon">
                 <i class="mdi mdi-history"></i>
               </span>
