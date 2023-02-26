5083fed20abcafc6a0b40874e08ee94cbc6fcace
Andrei Nowell Ong <andreinowellong@gmail.com>
2023-02-27 02:26:21 +0800

    Superadmin documents view

--- storage/framework/views/19c1454ee709fbb3128b67fdce68b94baa77b605.php
+++ storage/framework/views/19c1454ee709fbb3128b67fdce68b94baa77b605.php
@@ -605,6 +605,85 @@
                     </div>
                     </div>
                     
+                      <!-- Modal 2x2 -->
+                    <div class="modal fade" id="ViewModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content px-2 py-2">
+                        <div class="">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="image text-center">
+                           <img height="400" width="400" src="<?php echo e(url('uploads/'. 'merchant_documents'. '/'. $partner->merchant_id. '/'. $partner->logo)); ?>" alt="">
+                       </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+                     <!-- Modal vehicle -->
+                    <div class="modal fade" id="ViewModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                        <div class="modal-content px-2 py-2">
+                        <div class="">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="image text-center">
+                            <div id="carouselExampleContr" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                            <img class="d-block" height="650"  width="100%" src="<?php echo e(url('uploads/'. 'merchant_documents'. '/'. $partner->merchant_id. '/'. $partner->menu_photo)); ?>" alt="First slide">
+                            </div>
+        
+                        </div>
+                        
+                        </div>
+                           
+                       </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                       <!-- Modal vehicle -->
+                    <div class="modal fade" id="ViewModal3" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+                        <div class="modal-content px-2 py-2">
+                        <div class="">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="image text-center">
+                            <div id="carouselExampleControlss" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                            <img class="d-block" height="350"  width="100%" src="<?php echo e(url('uploads/'. 'merchant_documents'. '/'. $partner->merchant_id. '/'. 'valid id/'.$partner->front_license)); ?>" alt="First slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block" height="350" width="100%" src="<?php echo e(url('uploads/'. 'merchant_documents'. '/'. $partner->merchant_id. '/'. 'valid id/'.$partner->back_license)); ?>" alt="Second slide">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleControlss" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControlss" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                        </div>
+                           
+                       </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
 <!-- content-wrapper ends -->
 <!-- partial:../../partials/_footer.html -->
 <footer class="footer">
