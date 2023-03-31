9948c3bf7fe4ce0bfbc9fd3c2a92fd22a0ca594b
Troy Exclamado <troy.exclamado@dice205.com>
2023-03-31 11:24:32 +0800

    add product fix

--- app/Http/Controllers/Admin_product.php
+++ app/Http/Controllers/Admin_product.php
@@ -248,33 +248,6 @@ class Admin_product extends Controller
             $addProd->save();
             
             return redirect('product');
-
-
-            $tmp_file = TemporaryFile::where('folder', $request->product_image)->first();
-
-            if($tmp_file)
-            {
-                Storage::copy('posts/tmp/'. $tmp_file->folder. '/' . $tmp_file->file, 'posts/' .$tmp_file->folder . '/' .$tmp_file->file);
-
-                tbl_product::create([
-                    'merchant_id' => session('loginID'),
-                    'product_name' => $request->product_name,
-                    'stock' => $request->stock,
-                    'product_image' => $tmp_file->folder. '/' .$tmp_file->file_name,
-                    'price' => $request->price,
-                    'category_name' => $request->category,
-                    'status' => $request->status,
-                    'tags' => $request->tags_category,
-                    'description' => $request->description,
-                    'ingredients' => $request->ingredients
-                ]);
-                Storage::deleteDirectory('posts/tmp/'. $tmp_file->folder);
-                $tmp_file->delete();
-                return redirect('/')->with('success', 'Added Success.');
-            }
-            return redirect('/')->with('danger', 'Please Upload an Image.');
-
-
     }
 
 
