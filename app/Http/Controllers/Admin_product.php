d88c8308023ad75fc4d8f501bef2e341a18b8ba3
Troy Exclamado <troy.exclamado@dice205.com>
2023-04-22 01:23:09 +0800

    test query again

--- app/Http/Controllers/Admin_product.php
+++ app/Http/Controllers/Admin_product.php
@@ -441,7 +441,8 @@ class Admin_product extends Controller
 
 // Admin order Show the Table
     public function Orders(){
-        $orders = DB::table('tbl_orders')->where('restaurant_id', '=', session('loginID'))->selectRaw('MAX(order_id) as order_id, order_key, customer_id, merchant_id, product_id, restaurant_id, quantity, total, status, payment_type, latitude, longitude, date, created_at, updated_at')->groupBy('order_key')->get();
+        $orderKeys = tbl_orders::select('order_keys')->distinct()->where('restaurant_id', session('loginID'))->pluck('order_key')->toArray();
+        $orders = tbl_orders::whereIn('order_key', $orderKeys)->get();
         dd($orders);
         $orders = $orders->sortByDesc('order_id');
 
