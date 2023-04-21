d1ca91accacf14d944cfc7f7e4b9bc41a6397581
Troy Exclamado <troy.exclamado@dice205.com>
2023-04-22 01:30:23 +0800

    test display

--- app/Http/Controllers/Admin_product.php
+++ app/Http/Controllers/Admin_product.php
@@ -443,7 +443,6 @@ class Admin_product extends Controller
     public function Orders(){
         $orderKeys = tbl_orders::select('order_key')->distinct()->where('restaurant_id', session('loginID'))->pluck('order_key')->toArray();
         $orders = tbl_orders::whereIn('order_key', $orderKeys)->where('restaurant_id', session('loginID'))->get();
-        dd($orders);
         $orders = $orders->sortByDesc('order_id');
 
         $TotalOrders = DB::table('tbl_orders')->where('restaurant_id', '=', session('loginID'))->count();
