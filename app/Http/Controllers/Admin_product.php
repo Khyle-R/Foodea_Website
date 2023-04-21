2a3851993279817377e6f2faa0253c0d3c940161
Troy Exclamado <troy.exclamado@dice205.com>
2023-04-21 13:38:04 +0800

    sort orders desc

--- app/Http/Controllers/Admin_product.php
+++ app/Http/Controllers/Admin_product.php
@@ -382,7 +382,7 @@ class Admin_product extends Controller
 
 // Admin order Show the Table
     public function Orders(){
-        $orders = DB::table('tbl_orders')->where('restaurant_id', '=', session('loginID'))->get();
+        $orders = DB::table('tbl_orders')->where('restaurant_id', '=', session('loginID'))->sortByDesc('date')->get();
 
         $TotalOrders = DB::table('tbl_orders')->where('restaurant_id', '=', session('loginID'))->count();
         $PendingOrders = DB::table('tbl_orders')->where([['status','Pending'],['restaurant_id', '=', session('loginID')]])->count();
